9db80ef88c874724f02e6749b2713863b3e279d6
Giu27 <Caruso.giuseppe09@hotmail.com>
2026-04-10 23:15:40 +0200

    Circles grid

--- main.py
+++ main.py
@@ -9,13 +9,46 @@ pg.display.set_caption("Connect 4!")
 
 clock = pg.Clock()
 
-while True:
-    screen.fill(BACKGROUND_COLOR)
+grid = [[0 for column in range(COLUMNS)] for row in range(ROWS)]
 
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            pg.quit()
-            sys.exit()
+def drawGrid():
+    sideOffset = int(WIDTH * (SIDE_PADDING / 100))
+    topOffset = int(HEIGHT * (TOP_PADDING / 100))
+    bottomOffset = int(HEIGHT * (BOTTOM_PADDING / 100))
+    actualWidth = WIDTH - sideOffset * 2
+    actualHeight = HEIGHT - topOffset - bottomOffset
+    hStep = (actualWidth - 2 * RADIUS) / (COLUMNS - 1) if COLUMNS > 1 else 0
+    vStep = (actualHeight - 2 * RADIUS) / (ROWS - 1) if ROWS > 1 else 0
 
-    clock.tick(FPS)
-    pg.display.update()
+    
+    for r in range(ROWS):
+        for c in range(COLUMNS):
+            center = (RADIUS + sideOffset + hStep * c , RADIUS + topOffset  + vStep * r)
+            pg.draw.circle(screen, EMPTY_COLOUR, center, RADIUS)
+            if DEBUG: 
+                pg.draw.line(screen, "red", (0,10), (sideOffset, 10))
+                pg.draw.line(screen, "red", (WIDTH,10), (WIDTH - sideOffset, 10))
+                pg.draw.line(screen, "green", (sideOffset, 0), (sideOffset, topOffset))
+                pg.draw.line(screen, "green", (WIDTH - sideOffset, 0), (WIDTH - sideOffset, topOffset))
+                pg.draw.line(screen, "green", (sideOffset, HEIGHT), (sideOffset, HEIGHT - bottomOffset))
+                pg.draw.line(screen, "green", (WIDTH - sideOffset, HEIGHT), (WIDTH - sideOffset, HEIGHT - bottomOffset))
+                pg.draw.line(screen, "red", center, (center[0], 10))
+                pg.draw.line(screen, "green", (center[0] - RADIUS, center[1] - RADIUS), (center[0] + RADIUS, center[1] - RADIUS))
+                pg.draw.line(screen, "green", (center[0] - RADIUS, center[1] + RADIUS), (center[0] + RADIUS, center[1] + RADIUS))
+
+if __name__ == "__main__":
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                pg.quit()
+                sys.exit()
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    DEBUG = not DEBUG
+
+        screen.fill(BACKGROUND_COLOUR)
+
+        drawGrid()
+
+        clock.tick(FPS)
+        pg.display.update()
--- settings.py
+++ settings.py
@@ -1,6 +1,19 @@
 #General
 WIDTH, HEIGHT = 1920, 1080
 FPS = 60
+DEBUG = False
 
-#Colors
-BACKGROUND_COLOR = "lightgray"
+#Colours
+BACKGROUND_COLOUR = "lightgray"
+EMPTY_COLOUR = "white"
+PLAYER1_COLOUR = "red"
+PLAYER2_COLOUR = "blue"
+
+#Grid
+ROWS, COLUMNS = 4, 7
+SIDE_PADDING = 10 #This is a percentages
+TOP_PADDING = 10  #This as well
+BOTTOM_PADDING = 5 #And this
+
+#Circles
+RADIUS = 60
